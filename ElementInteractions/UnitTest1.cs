a9947e37c9d97b44483dabcaa4113ad22b187df7
nadvolod <nadvolod@gmail.com>
2017-02-03 11:33:47 -0500

    Created a new project for element identification
    Cleaned up the project
    Added chrome driver
    Built, tested

---
+++ ElementInteractions/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ElementInteractions
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
