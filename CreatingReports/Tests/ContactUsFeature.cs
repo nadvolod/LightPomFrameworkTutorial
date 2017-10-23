2f05b9909f2a3e1d87f0eeaebb310579813a2b85
nadvolod <nadvolod@gmail.com>
2017-10-22 20:10:38 -0400

    Deleted dead code tests

--- CreatingReports/Tests/ContactUsFeature.cs
+++ CreatingReports/Tests/ContactUsFeature.cs
@@ -32,24 +32,5 @@ namespace CreatingReports.Tests
             Assert.IsTrue(contactUsPage.IsLoaded, "The contact us page did not open successfully.");
             
         }
-
-
-
-        [TestMethod]
-        [TestProperty("Author", "NikolayAdvolodkin")]
-        public void TCID6()
-        {
-            //1. open automation page with many items - https://www.ultimateqa.com/complicated-page/
-            //2. Fill out and submit the form in the "Section of random stuff"
-            //3. validate that form was submitted successfully
-        }
-        [TestMethod]
-        [TestProperty("Author", "NikolayAdvolodkin")]
-        public void TCID7()
-        {
-            //1. open automation page with many items - https://www.ultimateqa.com/complicated-page/
-            //2. Perform a search for string "selenium errors"
-            //3. validate that correct search results were returned
-        }
     }
 }
