2fb421d6f92510bf9ad621c25dfa51fc13e3ebb9
nadvolod <nadvolod@gmail.com>
2018-01-06 18:10:37 -0500

    Added result logging to sauce labs tests and added NUnit instead

--- SauceLabs/SauseLabsTests.cs
+++ SauceLabs/SauseLabsTests.cs
@@ -1,11 +1,12 @@
 ﻿using AutomationResources;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
 using OpenQA.Selenium;
 
 namespace SauceLabs
 {
-    [TestClass]
-    [TestCategory("SauceLabsTests")]
+    [TestFixture]
+    [Category("SauceLabsTests")]
     public class SauseLabsTests
     {
         private IWebDriver Driver { get; set; }
@@ -13,7 +14,7 @@ namespace SauceLabs
         internal SampleApplicationPage SampleAppPage { get; private set; }
         internal TestUser EmergencyContactUser { get; private set; }
 
-        [TestMethod]
+        [Test]
         public void Test1()
         {
             SetGenderTypes(Gender.Female, Gender.Female);
@@ -23,7 +24,7 @@ namespace SauceLabs
             var ultimateQAHomePage = SampleAppPage.FillOutPrimaryContactFormAndSubmit(TheTestUser);
             AssertPageVisible(ultimateQAHomePage);
         }
-        [TestMethod]
+        [Test]
         public void PretendTestNumber2()
         {
             SampleAppPage.GoTo();
@@ -32,7 +33,7 @@ namespace SauceLabs
             AssertPageVisibleVariation2(ultimateQAHomePage);
         }
 
-        [TestMethod]
+        [Test]
         public void Test3()
         {
             SetGenderTypes(Gender.Other, Gender.Other);
@@ -42,14 +43,23 @@ namespace SauceLabs
             var ultimateQAHomePage = SampleAppPage.FillOutPrimaryContactFormAndSubmit(TheTestUser);
             AssertPageVisibleVariation2(ultimateQAHomePage);
         }
-        [TestCleanup]
+        [TearDown]
         public void CleanUpAfterEveryTestMethod()
         {
-            Driver.Close();
-            Driver.Quit();
+            var passed = TestContext.CurrentContext.Result.Outcome.Equals(ResultState.Success);
+            try
+            {
+                // Logs the result to Sauce Labs
+                ((IJavaScriptExecutor)Driver).ExecuteScript("sauce:job-result=" + (passed ? "passed" : "failed"));
+            }
+            finally
+            {
+                // Terminates the remote webdriver session
+                Driver?.Quit();
+            }
         }
 
-        [TestInitialize]
+        [SetUp]
         public void SetupForEverySingleTestMethod()
         {
             Driver = new WebDriverFactory().CreateSauceDriver();
@@ -64,15 +74,11 @@ namespace SauceLabs
             EmergencyContactUser.LastName = "Emergency Last Name";
         }
 
-        private static void AssertPageVisible(UltimateQAHomePage ultimateQAHomePage)
-        {
+        private static void AssertPageVisible(UltimateQAHomePage ultimateQAHomePage) => 
             Assert.IsTrue(ultimateQAHomePage.IsVisible, "UltimateQA home page was not visible.");
-        }
 
-        private static void AssertPageVisibleVariation2(UltimateQAHomePage ultimateQAHomePage)
-        {
+        private static void AssertPageVisibleVariation2(UltimateQAHomePage ultimateQAHomePage) => 
             Assert.IsFalse(!ultimateQAHomePage.IsVisible, "UltimateQA home page was not visible.");
-        }
 
         private void SetGenderTypes(Gender primaryContact, Gender emergencyContact)
         {
