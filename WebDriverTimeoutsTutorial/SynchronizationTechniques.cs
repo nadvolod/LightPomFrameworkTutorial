51b26e861c10854bcb5c8b8ed605be0a2d46d2b6
nadvolod <nadvolod@gmail.com>
2018-03-24 17:38:52 -0400

    updated chromedriver
    Updated chromedriver to 2.37

---
+++ WebDriverTimeoutsTutorial/SynchronizationTechniques.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using AutomationResources;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using WebDriverTimeoutsTutorial;
+//using static OpenQA.Selenium.Support.UI.ExpectedConditions;
+
+namespace WebdriverTimeoutsTutorial
+{
+    [TestClass]
+    [TestCategory("Explicit waits")]
+    public class SynchronizationTechniques
+    {
+        private IWebDriver _driver;
+        By ElementToWaitFor = By.Id("finish");
+
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _driver = new WebDriverFactory().Create(BrowserType.Chrome);
+        }
+
+        [TestCleanup]
+        public void Teardown()
+        {
+            _driver.Close();
+            _driver.Quit();
+        }
+        [TestMethod]
+        public void Test1()
+        {
+            _driver.Navigate().GoToUrl(URL.SlowAnimationUrl);
+            //sync here
+
+            FillOutCreditCardInfo();
+            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(15));
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("go"))).Click();
+            Assert.IsTrue(wait.Until(ExpectedConditions.ElementIsVisible(By.Id("success"))).Displayed);
+        }
+        private void FillOutCreditCardInfo()
+        {
+            _driver.FindElement(By.Id("name")).SendKeys("test name");
+            _driver.FindElement(By.Id("cc")).SendKeys("1234123412341234");
+            _driver.FindElement(By.Id("month")).SendKeys("01");
+            _driver.FindElement(By.Id("year")).SendKeys("2020");
+        }
+        [TestMethod]
+        public void Test3_ExplicitWait_HiddenElement()
+        {
+            _driver.Navigate().GoToUrl(URL.HiddenElementUrl);
+            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(5));
+            wait.Until(ExpectedConditions.ElementToBeClickable(ElementToWaitFor)).Click();
+        }
+        [TestMethod]
+        public void Test4_ExplicitWait_RenderedAfter()
+        {
+            _driver.Navigate().GoToUrl(URL.ElementRenderedAfterUrl);
+            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(5));
+            wait.Until(ExpectedConditions.ElementToBeClickable(ElementToWaitFor)).Click();
+        }
+        //Quiz
+        [TestMethod]
+        public void Test3_ExplicitWaitOptions_HiddenElement()
+        {
+            _driver.Navigate().GoToUrl(URL.HiddenElementUrl);
+            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(15));
+            wait.Until(ExpectedConditions.ElementExists(ElementToWaitFor)).Click();
+        }
+    }
+}
