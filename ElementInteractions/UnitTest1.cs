d682fc4cf9433539292eab052773f8d9e19eac5c
nadvolod <nadvolod@gmail.com>
2019-03-23 15:53:20 -0400

    Trial of drag and drop test case

--- ElementInteractions/UnitTest1.cs
+++ ElementInteractions/UnitTest1.cs
@@ -17,18 +17,20 @@ namespace ElementInteractions
             var driver = new WebDriverFactory().Create(BrowserType.Chrome);
             var actions = new Actions(driver);
 
-            driver.Navigate().GoToUrl("http://localhost:3000/");
+            //driver.Navigate().GoToUrl("http://localhost:3000/");
 
             //var item1 = driver.FindElement(By.Id("1"));
             //var dropLocation = driver.FindElement(By.ClassName("done"));
             //actions.DragAndDrop(item1, dropLocation).Perform();
             ////actions.ClickAndHold(item1).MoveToElement(dropLocation).Release().Perform(); 
 
-            //driver.Navigate().GoToUrl("https://jqueryui.com/droppable/");
-            //var draggable = driver.FindElement(By.Id("draggable"));
-            //var drop = driver.FindElement(By.Id("droppable"));
-            ////actions.DragAndDrop(draggable, drop).Perform();
-            //actions.ClickAndHold(draggable).MoveToElement(drop).Release().Perform();
+            driver.Navigate().GoToUrl("https://jqueryui.com/droppable/");
+            var frame = driver.FindElement(By.ClassName("demo-frame"));
+            driver.SwitchTo().Frame(frame);
+             draggable = driver.FindElement(By.Id("draggable"));
+             drop = driver.FindElement(By.Id("droppable"));
+            //actions.DragAndDrop(draggable, drop).Perform();
+            actions.ClickAndHold(draggable).MoveToElement(drop).Release().Perform();
 
             driver.Navigate().GoToUrl("http://demo.guru99.com/test/drag_drop.html");
             draggable = driver.FindElement(By.XPath("//*[@id='credit2']/a"));
