dfbcdd269390c7e1c18e82c6c95fb56d888fc6f2
XITIJTHOOL <62694482+XITIJTHOOL@users.noreply.github.com>
2020-05-10 20:48:19 +0530

    Updated all code
    Hello nadvolod,
     I am following along the code by watching the video. And preparing the suggestion document indicating the changed made with improvements. I will upload it and give you the link once the course is over. so stay tuned. Meantime, I figured out there are many code misplaced God knows why. I know once the organization go huge the time spent on critical things are less and procrastination made the matter worse. Don't worry Bro, I got your back. I am trying my  code as your have shown in the video. Now , I have doubled checked the code it works.
    bye.
    The functions added are shown in the video 262. Drag and drop quiz answer 
    everything is good tested twice.
    bye.

--- ElementInteractions/UnitTest1.cs
+++ ElementInteractions/UnitTest1.cs
@@ -1,23 +1,33 @@
-﻿using System;
+using System;
 using AutomationResources;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+
+using ExpectedConditions = SeleniumExtras.WaitHelpers.ExpectedConditions;
 
 namespace ElementInteractions
 {
-    [TestClass]
+
+    [TestFixture]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-            IWebElement draggable;
-            IWebElement drop;
-            var driver = new WebDriverFactory().Create(BrowserType.Chrome);
-            var actions = new Actions(driver);
+        private IWebDriver _driver;
+        private Actions _actions;
+        private WebDriverWait _wait;
 
-            driver.Navigate().GoToUrl("http://localhost:3000/");
+
+        [Test]
+        public void DragDropExample1()
+        {
+            _driver.Navigate().GoToUrl("https://jqueryui.com/droppable/");
+            _wait.Until(ExpectedConditions.FrameToBeAvailableAndSwitchToIt(By.ClassName("demo-frame")));
+            IWebElement sourceElement = _driver.FindElement(By.Id("draggable"));
+            IWebElement targetElement = _driver.FindElement(By.Id("droppable"));
+            _actions.DragAndDrop(sourceElement, targetElement).Perform();
+
+            Assert.AreEqual("Dropped!", targetElement.Text);
 
             //var item1 = driver.FindElement(By.Id("1"));
             //var dropLocation = driver.FindElement(By.ClassName("done"));
@@ -30,13 +40,66 @@ namespace ElementInteractions
             ////actions.DragAndDrop(draggable, drop).Perform();
             //actions.ClickAndHold(draggable).MoveToElement(drop).Release().Perform();
 
-            driver.Navigate().GoToUrl("http://demo.guru99.com/test/drag_drop.html");
-            draggable = driver.FindElement(By.XPath("//*[@id='credit2']/a"));
-            drop = driver.FindElement(By.XPath("//*[@id='bank']/li"));
-            //actions.DragAndDrop(draggable, drop).Perform();
-            actions.ClickAndHold(draggable).MoveToElement(drop).Release().Perform(); 
+            //driver.Navigate().GoToUrl("http://demo.guru99.com/test/drag_drop.html");
+            //draggable = driver.FindElement(By.XPath("//*[@id='credit2']/a"));
+            //drop = driver.FindElement(By.XPath("//*[@id='bank']/li"));
+            ////actions.DragAndDrop(draggable, drop).Perform();
+            //actions.ClickAndHold(draggable).MoveToElement(drop).Release().Perform();
 
-            driver.Quit();
+           
+        }
+
+        [Test]
+        public void DragDropExample2()
+        {
+
+            _driver.Navigate().GoToUrl("https://jqueryui.com/droppable/");
+            _wait.Until(ExpectedConditions.FrameToBeAvailableAndSwitchToIt(By.ClassName("demo-frame")));
+
+            IWebElement sourceElement = _driver.FindElement(By.Id("draggable"));
+            IWebElement targetElement = _driver.FindElement(By.Id("droppable"));
+            var drag = _actions
+                .ClickAndHold(sourceElement)
+                .MoveToElement(targetElement)
+                .Release(targetElement)
+                .Build();
+
+            drag.Perform();
+
+            Assert.AreEqual("Dropped!", targetElement.Text);
+
+        }
+
+        [Test]
+        public void jQueryDragDropQuiz()
+        {
+            _driver.Navigate().GoToUrl("http://www.pureexample.com/jquery-ui/basic-droppable.html");
+            _wait.Until(ExpectedConditions.FrameToBeAvailableAndSwitchToIt(By.XPath("//*[@id='ExampleFrame-94']")));
+
+            //var sourceLocator = _driver.FindElement(By.ClassName("//*[@class='square ui-draggable']"));
+            var sourceLocator = _driver.FindElement(By.XPath("//*[@class='square ui-draggable']"));
+            var destinationLocator = _driver.FindElement(By.XPath("//*[@class='squaredotted ui-droppable']"));
+            _actions.DragAndDrop(sourceLocator, destinationLocator).Build().Perform();
+
+            var droppedText = _driver.FindElement(By.XPath("//*[@id='info']")).Text;
+
+            Assert.AreEqual("dropped!", droppedText);
+
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            _driver = new WebDriverFactory().Create(BrowserType.Chrome);
+            _actions = new Actions(_driver);
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(15));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _driver.Close();
+            _driver.Quit();
         }
     }
 }
