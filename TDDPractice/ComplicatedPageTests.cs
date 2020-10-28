b141e711ba9d72c2e464372261cd406a39bad518
Andreea <andreea.draniceanu@gmail.com>
2020-10-28 07:52:03 +0200

    Updated tests

--- TDDPractice/ComplicatedPageTests.cs
+++ TDDPractice/ComplicatedPageTests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using AutomationResources;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System.IO;
 using System.Reflection;
@@ -9,7 +11,8 @@ namespace TDDPractice
     [TestCategory("TDDPractice")]
     public class ComplicatedPageTests
     {
-        
+        private IWebDriver driver;
+
         public void TestMethod1()
         {
             var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
@@ -24,9 +27,7 @@ namespace TDDPractice
         [TestMethod]
         public void TestMethodNew()
         {
-            var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            var driver = new ChromeDriver(path);
-
+            driver = new WebDriverFactory().Create(BrowserType.Chrome);
             var complicatedPage = new ComplicatedPage(driver);
             complicatedPage.Open();
             complicatedPage.SearchArticles("automation testing");
