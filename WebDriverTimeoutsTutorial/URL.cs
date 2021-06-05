aa7ca3eadc6f0ce63415d3265f672b205dfde24a
andreea-qa <andreea.draniceanu@gmail.com>
2021-06-05 12:29:07 +0300

    Updated explicit wait test with new URL

--- WebDriverTimeoutsTutorial/URL.cs
+++ WebDriverTimeoutsTutorial/URL.cs
@@ -11,6 +11,6 @@ namespace WebDriverTimeoutsTutorial
         public static string HiddenElementUrl => "https://the-internet.herokuapp.com/dynamic_loading/1";
 
         public static string ElementRenderedAfterUrl => "https://the-internet.herokuapp.com/dynamic_loading/2";
-        public static string SlowAnimationUrl => "http://awful-valentine.com/purchase-forms/slow-animation/";
+        public static string SlowAnimationUrl => "https://the-internet.herokuapp.com/dynamic_controls";
     }
 }
